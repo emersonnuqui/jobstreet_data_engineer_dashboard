f3c0aa6800d6059b9d303481c6f72646ee24f46d
unknown <emersonnuqui@gmail.com>
2023-10-02 08:04:32 -0700

    Updated code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import dash
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc 
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
@@ -7,8 +8,68 @@ from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
+# Set the background color for the entire HTML body
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body style="background-color: #004AAD;">
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+'''
+
+# Define custom CSS to import the font
+custom_css = [
+    html.Link(
+        rel="stylesheet",
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500;600;700;800&display=swap"
+    )
+]
+
 app.layout = html.Div([
-  html.H1('Sales breakdowns')])
+    html.Div([html.H1('JOBSTREET DATA ENGINEER DASHBOARD', 
+                      style={'font-family': 'Open Sans, sans-serif', 'font-weight': '800', 'text-align': 'center', 'font-size': '63px', 'color':'white'})]),
+
+    html.Div([html.P("Data Engineering has been emerging throughout this year as ", 
+                     style={'font-family': 'Open Sans, sans-serif', 'font-weight': '300', 'text-align': 'center', 'color':'white'})]),
+    html.P("""Data Engineering has been emerging throughout this year as """, 
+           style={'font-family': 'Open Sans, sans-serif', 'font-weight': '300', 'text-align': 'center', 'color':'white'}),
+
+
+    html.Div(
+        children=[
+            html.Div(
+                dbc.Card(
+                    dbc.CardBody([
+                        html.H1("Number of Jobs", className="card-title", 
+                                style={'font-family': 'Open Sans, sans-serif', 'font-weight': '500', 'text-align': 'center', 'font-size': '20px', 'color':'#000000', 'margin-top':'20px'}),
+
+                        html.P("This is some text within the card.", className="card-text",  style={'font-family': 'Open Sans, sans-serif', 'font-weight': '800', 'text-align': 'center', 'font-size':'24px', 'color':'#000000'}),
+                    ], style={'margin-top':'20px'}), style={'background-color': '#00BF63',  'margin':'50px'}),
+                style={'width': '20%', 'position': 'relative', 'display': 'inline-block'}
+            ),
+
+            html.Div(
+                "Column 2",
+                style={'width': '40%', 'display': 'inline-block'}
+            ),
+            html.Div(
+                "Column 3",
+                style={'width': '40%', 'display': 'inline-block'}
+            ),
+    ])
+  ])
 
 
 if __name__ == '__main__':
--- pipelines/load.py
+++ pipelines/load.py
@@ -6,36 +6,26 @@ import time
 import os
 import re
 from tqdm import tqdm 
+import boto3
+from io import StringIO
 
-import mysql.connector
 
-def set_mysql_connection(host, user, password, database):
-    conn = mysql.connector.connect(
-        host = host,
-        user = user,
-        password = password,
-        database = database
-    )
+def connect_to_s3(aws_key_id, secret_key):
+    # Set up S3 access
+    s3 = boto3.client('s3',                                # AWS Service Name
+					region_name = 'ap-southeast-2',           # Region; geo-region where our resources are located.
+                    aws_access_key_id = aws_key_id,         # Key
+                    aws_secret_access_key = secret_key)  # Secret Key
+    return s3
+
+
+def export_main_data(main_data, new_data, s3, bucket_name):
+    response = s3.get_object(Bucket=bucket_name, Key=main_data)
+    csv_data = response['Body'].read().decode('utf-8')
+
+    # Load the CSV data into a Pandas DataFrame
+    df = pd.read_csv(StringIO(csv_data))
     
-    mycursor = conn.cursor()
+    updated_data = pd.merge([df, new_data], ignore_index=True)
+    updated_data = updated_data.drop_duplicates(subset='id')
     
-    return conn, mycursor
-
-def insert_into_database(data, table, mycursor, conn):
-    for i, row in tqdm(data.iterrows()):
-        try:
-            query = f"""
-              INSERT INTO {table} (job_id, job_title, location, company, salary, job_url, qualification,
-                                   year_of_experience, job_type, company_size, tech_language, tech_database,
-                                   tech_cloud, tech_web_framework, tech_libraries, tech_developer, tech_data_viz,
-                                   tech_version_control)
-                        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-              """
-            info = (row['job_id'], row['title'], row['location'], row['company'], row['salary'], row['job_url'],
-                   row['Qualification'], row['Years of Experience'], row['Job Type'], row['Company Size'],
-                   row['languages'], row['databases'], row['cloud_platforms'], row['web_frameworks'], row['libraries'],
-                   row['developers'], row['data_visualization'], row['version_controls'])
-            mycursor.execute(query, info)
-            conn.commit()
-        except:
-            pass
